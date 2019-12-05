a9fad99b46421c64b6397ebcb55c5db7410824d4
Chris H <holtchr12@gmail.com>
2019-12-04 19:40:34 -0600

    finished all bonus missions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function() {
     this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
         response.json().then( function(json) {
             let container = document.getElementById("container");
+            json.sort(function(a, b) {return b.hoursInSpace-a.hoursInSpace});
             for (let i = 0; i < json.length; i++) {
                 let color;
                 if (json[i].active === true) {
